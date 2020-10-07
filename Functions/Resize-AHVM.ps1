13f8702c01fe57b57e755d03643f2f9e08a35b4b
PaulHCode <pauleharrison@gmail.com>
2020-10-07 15:22:55 -0400

    comments/verbose

--- Functions/Resize-AHVM.ps1
+++ Functions/Resize-AHVM.ps1
@@ -1,4 +1,32 @@
 Function Resize-AHVM {
+    <#
+.SYNOPSIS
+    Resizes a VM.
+.DESCRIPTION
+    Resizes a VM to $NewVMSize
+.PARAMETER SubscriptionId
+    If the VM is not in the current subscription specify the subsctiptionId of the subscription it is in here
+.PARAMETER VMName
+    The name of the VM to resize
+.PARAMETER ResourceGroup
+    The resource group the VM is in
+.PARAMETER NewVMSize
+    The size the VM should be resized to
+.PARAMETER AutoStart
+    Turns the VM on after the resize
+.PARAMETER Force
+    Deallocates the VM to resize it if it is currently not deallocated
+.EXAMPLE
+    Remove-AHPolicyToReport -PolicyDefinitionID '/providers/Microsoft.Authorization/policyDefinitions/0015ea4d-51ff-4ce3-8d8c-f3f8f0179a56'
+.INPUTS
+    String
+.OUTPUTS
+.NOTES
+    Author:  Paul Harrison
+.LINK
+
+#>
+
     [CmdletBinding()]
     param (
         [Parameter()]
@@ -35,6 +63,12 @@ Function Resize-AHVM {
     }
     catch { throw }
 
+    If ($NewVMSize -eq $VM.HardwareProfile.VmSize) {
+        Write-Verbose "$($VM.Name) is already a $NewVMSize - no changes made"
+        return
+    }
+
+    Write-Verbose "Verifying the VM is deallocated"
     If (!(($VMStatus.Statuses.code -like "*PowerState/*" -like "*deallocated"))) {
         If ($Force) {
             Stop-AzVM -ResourceGroupName  $VM.ResourceGroupName -Name $VM.Name -Force -NoWait
@@ -47,15 +81,11 @@ Function Resize-AHVM {
             Throw "The VM $($VM.Name) is currently not dealloated.  Unable to resize."
         }    
     }
-    #    Write-Host "VM ="
-    #    $VM | Format-List *
-    #    Write-Host "Location = $($VM.Location)"
-    #    write-host "VM Size = $($VM.HardwareProfile.VmSize)"
+
+    Write-Verbose "Verifying $NewVMSize is allowed in $($VM.Location)"
     If ((Get-AzVMSize -Location $($VM.Location)).Name -notcontains $NewVMSize) {
         Throw "Invalid VM Size for $($VM.Location)"  
     }
-
-    #    If()
 
     $VM.HardwareProfile.VmSize = $NewVMSize
 
