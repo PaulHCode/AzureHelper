b543dd9e317d82ad293d74fed35b6a1ff8b53eb6
Agustin Gallegos [MSFT] <62895191+agallego-css@users.noreply.github.com>
2021-05-25 11:20:36 -0300

    Update Get-AHMyPublicIPAddress.ps1
    A possible way to validate an IP address, is to Type-casting to [System.Net.IPAddress] Class.
    Check method 3 in this article: https://ridicurious.com/2018/11/14/4-ways-to-validate-ipaddress-in-powershell/

--- Functions/Get-AHMyPublicIPAddress.ps1
+++ Functions/Get-AHMyPublicIPAddress.ps1
@@ -1,4 +1,9 @@
-Function Get-AHMyPublicIPAddress {
-    (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
-    #I really should do data validation to make sure that an IP is returned, or at least make sure something was returned.
+Function Get-AHMyPublicIPAddress {    
+    # capturing the response
+    $response = Invoke-RestMethod -Uri "http://ifconfig.me/ip"
+    
+    # casting response to IPAddress class and get the result as a Bool
+    if ( $response -as [IPAddress] -as [Bool] ) {
+        return $response
+    }
 }
