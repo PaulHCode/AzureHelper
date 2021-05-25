dfef66307c197d323ac7956da548f84c19a832db
PaulHCode <71329201+PaulHCode@users.noreply.github.com>
2021-05-25 19:43:06 -0400

    Update Get-AHMyPublicIPAddress.ps1
    throws an error if it can't determine the IP address

--- Functions/Get-AHMyPublicIPAddress.ps1
+++ Functions/Get-AHMyPublicIPAddress.ps1
@@ -1,9 +1,10 @@
 Function Get-AHMyPublicIPAddress {    
-    # capturing the response
     $response = Invoke-RestMethod -Uri "http://ifconfig.me/ip"
     
     # casting response to IPAddress class and get the result as a Bool
     if ( $response -as [IPAddress] -as [Bool] ) {
         return $response
+    }else {
+        throw 'Error getting your public IP address'
     }
 }
