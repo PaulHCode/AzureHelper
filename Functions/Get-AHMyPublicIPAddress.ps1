05e7afde9cc7c41eed889530a73c794de48407b2
PaulHCode <pauleharrison@gmail.com>
2021-05-25 19:36:42 -0400

    add validation on public ip address

--- Functions/Get-AHMyPublicIPAddress.ps1
+++ Functions/Get-AHMyPublicIPAddress.ps1
@@ -1,4 +1,26 @@
 Function Get-AHMyPublicIPAddress {
-    (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
-    #I really should do data validation to make sure that an IP is returned, or at least make sure something was returned.
+    <#
+.SYNOPSIS
+    Gets the public IP that Azure would see from whatever computer you run this on.
+
+.DESCRIPTION
+    Gets the public IP that Azure would see from whatever computer you run this on.  
+
+.EXAMPLE
+    Get-AHMyPublicIPAddress
+
+.OUTPUTS
+    String
+
+.NOTES
+    Author:  Paul Harrison
+#>
+
+    $MyIP = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
+    If ($MyIP -as [IPAddress] -as [Bool]) {
+        $MyIP
+    }
+    else {
+        throw 'Error getting your public IP address.'
+    }
 }
