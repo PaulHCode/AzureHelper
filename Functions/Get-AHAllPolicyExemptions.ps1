fb4f449e531dd11f2f48bb808d279d49a0b4c877
PaulHCode <Pauleharrison@gmail.com>
2023-06-07 16:12:38 -0400

    updated help for get-ahallpolicyexemtions

--- Functions/Get-AHAllPolicyExemptions.ps1
+++ Functions/Get-AHAllPolicyExemptions.ps1
@@ -4,10 +4,33 @@
     .EXAMPLE
         Get-AHAllPolicyExemptions
         Gets all policy exemptions for all management groups, subscriptions, resource groups, and individual resources.
+    .EXAMPLE
+        $Exemptions = Get-AHAllPolicyExemptions
+        Gets all policy exemptions
+    .EXAMPLE
+        $Exemptions = Get-AHAllPolicyExemptions | Where{$_.Properties.PolicyDefinitionReferenceIds.Count -eq 0}
+        Gets all policy exemptions then returns the ones that are applied to all policies, not just a subset of the policies within the .
+    .EXAMPLE
+        $Exemptions = Get-AHAllPolicyExemptions | Where{$_.Properties.ExpiresOn -lt [datetime]::Now}
+        Gets all policy exemptions then returns the ones that have expired.
+    .EXAMPLE
+        $Exemptions = Get-AHAllPolicyExemptions | Where{$_.Properties.ExpiresOn -gt ([datetime]::Now).AddDays(365) -or '' -eq $_.Properties.ExpiresOn}
+        Gets all policy exemptions then returns the ones that will expire over 1 year from now or have no expiration date.
 #>
 function Get-AHAllPolicyExemptions {
     [CmdletBinding()]
-    param ( )
+    param ( 
+        <# #Maybe I'll add in switches to make it easier later, until then there are examples
+        [switch]
+        $AppliedToAllPolicies,
+        [switch]
+        $ExcludeExpired,
+        [switch]
+        $IncludeExpired,
+        [switch]
+        $something
+#>
+    )
     Begin {
 
     }
