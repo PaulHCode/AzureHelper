2687f80e5dc20054a3a3972adc8786a3851e3491
PaulHCode <Pauleharrison@gmail.com>
2023-07-06 15:58:56 -0400

    Made write-ahloganalytics work in all environments

--- AzureHelper.psd1
+++ AzureHelper.psd1
@@ -12,7 +12,7 @@
     RootModule        = '.\AzureHelper.psm1'
 
     # Version number of this module.
-    ModuleVersion     = '1.0.45'
+    ModuleVersion     = '1.0.46'
 
     # Supported PSEditions
     # CompatiblePSEditions = @()
--- Functions/Write-AHLogAnalytics.ps1
+++ Functions/Write-AHLogAnalytics.ps1
@@ -54,57 +54,87 @@ Function Write-AHLogAnalytics {
         [string]$json
     )
 
-    $TimeStampField = Get-Date $($TimeStampInput.ToUniversalTime()) -Format "o"
-    $CustomerId = $WorkspaceId
-
-    # Create the function to create the authorization signature
-    Function Build-Signature ($customerId, $sharedKey, $date, $contentLength, $method, $contentType, $resource) {
-        $xHeaders = "x-ms-date:" + $date
-        $stringToHash = $method + "`n" + $contentLength + "`n" + $contentType + "`n" + $xHeaders + "`n" + $resource
-
-        $bytesToHash = [Text.Encoding]::UTF8.GetBytes($stringToHash)
-        $keyBytes = [Convert]::FromBase64String($sharedKey)
-
-        $sha256 = New-Object System.Security.Cryptography.HMACSHA256
-        $sha256.Key = $keyBytes
-        $calculatedHash = $sha256.ComputeHash($bytesToHash)
-        $encodedHash = [Convert]::ToBase64String($calculatedHash)
-        $authorization = 'SharedKey {0}:{1}' -f $customerId, $encodedHash
-        return $authorization
-    }
-
-    # Create the function to create and post the request
-    Function Post-LogAnalyticsData($customerId, $sharedKey, $body, $logType) {
-        $method = "POST"
-        $contentType = "application/json"
-        $resource = "/api/logs"
-        $rfc1123date = [DateTime]::UtcNow.ToString("r")
-        $contentLength = $body.Length
-        $signature = Build-Signature `
-            -customerId $customerId `
-            -sharedKey $sharedKey `
-            -date $rfc1123date `
-            -contentLength $contentLength `
-            -method $method `
-            -contentType $contentType `
-            -resource $resource
-        $uri = "https://" + $customerId + ".ods.opinsights.azure.com" + $resource + "?api-version=2016-04-01"
-
-        $headers = @{
-            "Authorization"        = $signature;
-            "Log-Type"             = $logType;
-            "x-ms-date"            = $rfc1123date;
-            "time-generated-field" = $TimeStampField;
+    begin {
+        switch ((Get-AzContext).Environment.Name) {
+            'AzureCloud' {
+                $odsEndpoint = 'ods.opinsights.azure.com'
+                break
+            }
+            'AzureChinaCloud' {
+                $odsEndpoint = 'ods.opinsights.azure.cn'
+                break
+            }
+            'AzureUSGovernment' {
+                $odsEndpoint = 'ods.opinsights.azure.us'
+                break
+            }
+            'AzureGermanCloud' {
+                $odsEndpoint = 'ods.opinsights.azure.de'
+                break
+            }
+            default {
+                throw 'Unknown Azure Environment'
+            }
         }
+        #$odsEndpoint
+            
+        # Create the function to create and post the request
+        Function Post-LogAnalyticsData($customerId, $sharedKey, $body, $logType) {
+            $method = "POST"
+            $contentType = "application/json"
+            $resource = "/api/logs"
+            $rfc1123date = [DateTime]::UtcNow.ToString("r")
+            $contentLength = $body.Length
+            $signature = Build-Signature `
+                -customerId $customerId `
+                -sharedKey $sharedKey `
+                -date $rfc1123date `
+                -contentLength $contentLength `
+                -method $method `
+                -contentType $contentType `
+                -resource $resource
+            $uri = "https://" + $customerId + '.' + $odsEndpoint + $resource + "?api-version=2016-04-01"
 
-        $response = Invoke-WebRequest -Uri $uri -Method $method -ContentType $contentType -Headers $headers -Body $body -UseBasicParsing
-        return $response.StatusCode
+            $headers = @{
+                "Authorization"        = $signature;
+                "Log-Type"             = $logType;
+                "x-ms-date"            = $rfc1123date;
+                "time-generated-field" = $TimeStampField;
+            }
 
+            $response = Invoke-WebRequest -Uri $uri -Method $method -ContentType $contentType -Headers $headers -Body $body -UseBasicParsing
+            return $response.StatusCode
+
+        }
+        
+        # Create the function to create the authorization signature
+        Function Build-Signature ($customerId, $sharedKey, $date, $contentLength, $method, $contentType, $resource) {
+            $xHeaders = "x-ms-date:" + $date
+            $stringToHash = $method + "`n" + $contentLength + "`n" + $contentType + "`n" + $xHeaders + "`n" + $resource
+
+            $bytesToHash = [Text.Encoding]::UTF8.GetBytes($stringToHash)
+            $keyBytes = [Convert]::FromBase64String($sharedKey)
+
+            $sha256 = New-Object System.Security.Cryptography.HMACSHA256
+            $sha256.Key = $keyBytes
+            $calculatedHash = $sha256.ComputeHash($bytesToHash)
+            $encodedHash = [Convert]::ToBase64String($calculatedHash)
+            $authorization = 'SharedKey {0}:{1}' -f $customerId, $encodedHash
+            return $authorization
+        }
     }
 
-    # Submit the data to the API endpoint
-    $result = Post-LogAnalyticsData -customerId $customerId -sharedKey $sharedKey -body ([System.Text.Encoding]::UTF8.GetBytes($json)) -logType $logType -TimeStampField $TimeStampField
-    If ($result -ne 200) {
-        Write-Error "Error writing to log analytics: $result"
+    Process {
+        $TimeStampField = Get-Date $($TimeStampInput.ToUniversalTime()) -Format "o"
+        $CustomerId = $WorkspaceId
+
+        # Submit the data to the API endpoint
+        $result = Post-LogAnalyticsData -customerId $customerId -sharedKey $sharedKey -body ([System.Text.Encoding]::UTF8.GetBytes($json)) -logType $logType -TimeStampField $TimeStampField
+        If ($result -ne 200) {
+            Write-Error "Error writing to log analytics: $result"
+        }
+    }
+    End {
+
     }
 }
