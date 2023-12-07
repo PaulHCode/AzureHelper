7d60476fce8f2dcacdc3aa96161c98aa8d2eb7f9
PaulHCode <Pauleharrison@gmail.com>
2023-12-07 08:42:33 -0500

    asdf

--- Functions/New-AHPIMRequest.ps1
+++ Functions/New-AHPIMRequest.ps1
@@ -1,3 +1,4 @@
+<#
 $myEnvironment = get-azcontext
 $graphUri = $myEnvironment.environment.ExtendedProperties.MicrosoftGraphUrl
 if($Null -eq $graphUri) {throw 'You must connect-azaccount first.'}
@@ -20,3 +21,5 @@ function getGroupsICanPIMInto() {
     if($Null -eq $graphUri) {throw 'You must connect-azaccount first.'}
     $token = (get-azaccesstoken -resourceurl $GraphResourceUrl).token
     $groupsICanPIMTo = Invoke-DCMsGraphQuery -AccessToken $token -GraphMethod 'POST' -GraphUri "$GraphResourceUrl/v1.0/me/getMemberGroups" -GraphBody '{"securityEnabledOnly": true}'
+
+#>
