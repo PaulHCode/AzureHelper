d4b95979ac88a04a5f33abcdc2d2f3497a1d095c
PaulHCode <Pauleharrison@gmail.com>
2024-01-05 10:07:17 -0500

    added tenant name to select-ahsubscription

--- Functions/Select-AHSubscription.ps1
+++ Functions/Select-AHSubscription.ps1
@@ -15,13 +15,13 @@ Function Select-AHSubscription {
 .LINK
 #>
     Test-AHEnvironment
-    If (!((Get-AzSubscription) -is [array])) { write-warning "The only subscription is currently selected."; return }
+    If (!((Get-AzSubscription) -is [array])) { Write-Warning "The only subscription is currently selected."; return }
     If ('System.Management.Automation.ServerRemoteDebugger' -eq [System.Management.Automation.Runspaces.Runspace]::DefaultRunspace.Debugger.GetType().FullName) {
         #throw "This cmdlet can only be used on a local host and cannot be used from a remote session."
         $choice = Read-Choice -Message "Select the subscription to use" -Choices $(Get-AzSubscription).Name
         $sub = (Get-AzSubscription)[$choice]
     }
-    elseif ((get-item env:/).Name -contains 'AZURE_HTTP_USER_AGENT') {
+    elseif ((Get-Item env:/).Name -contains 'AZURE_HTTP_USER_AGENT') {
         #throw "This cmdlet can only be used on a local host and cannot be used from Azure Cloud Shell."
         $choice = Read-Choice -Message "Select the subscription to use" -Choices $(Get-AzSubscription).Name
         $sub = (Get-AzSubscription)[$choice]
@@ -29,9 +29,9 @@ Function Select-AHSubscription {
     Else {
         $sub = $Null
         While ($Null -eq $sub -or $sub -is [array]) {
-            $sub = (Get-AzSubscription | Select-Object Name, Id, State | Out-GridView -PassThru -Title "Select the subscription to use")
+            $tenantLookup = (Get-AzTenant) | ForEach-Object { @{$_.id = $_.Name } }
+            $sub = (Get-AzSubscription | Select-Object Name, Id, State, @{n = 'TenantName'; e = { $tenantLookup."$($_.TenantId)" } } , TenantId | Out-GridView -PassThru -Title "Select the subscription to use")
         }
     }
     Set-AzContext $($sub.id) 
-    
 }
